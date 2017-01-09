2f009f120b82feea85e10e4fa8093c56f4a279d2
David Carlos <ddavidcarlos1392@gmail.com>
2017-01-09 10:34:23 -0200

    Fix table columns.

--- apply_models.R
+++ apply_models.R
@@ -18,7 +18,6 @@ cwe457_cubic_model <- function(modules) {
 # Read data and handle some usefull variables
 data <- read.csv("cwe.csv", header=TRUE, sep=',')
 
-# Calculation of CWE tax per module
 n_cwe476 = data$CWE476
 n_modules = data$Modules
 n_cwe457 = data$CWE457
@@ -34,12 +33,14 @@ cwe_476_tax1 = (1.911224*10^(-15))*modules1^3 - (1.72028*10^(-10))*modules1^2 + 
 cwe_476_tax2 = (1.911224*10^(-15))*modules2^3 - (1.72028*10^(-10))*modules2^2 + (4.85747*10^(-6))*modules2 - (0.03460173)
 
 i=1
-real_taxes <- vector(mode="numeric", length=0)
+real_cwe476_taxes <- vector(mode="numeric", length=0)
+real_cwe457_taxes <- vector(mode="numeric", length=0)
 cwe476_model_taxes <- vector(mode="numeric", length=0)
 cwe457_model_taxes <- vector(mode="numeric", length=0)
 
 for (cwe in data$CWE476){
-	real_taxes[i] <- c(n_cwe476[i]/n_modules[i])
+	real_cwe476_taxes[i] <- c(n_cwe476[i]/n_modules[i])
+	real_cwe457_taxes[i] <- c(n_cwe457[i]/n_modules[i])
 	cwe476_model_taxes[i] <- cwe476_cubic_model(n_modules[i])
 	cwe457_model_taxes[i] <- cwe457_cubic_model(n_modules[i])
 	i=i + 1
@@ -47,13 +48,13 @@ for (cwe in data$CWE476){
 kernel_version = c('4.0', '4.9')
 cwe476 = c(n_cwe476[1], n_cwe476[2])
 
-df = data.frame(kernel_version, n_cwe476, n_modules, cwe476_model_taxes, real_taxes)
+df = data.frame(kernel_version, n_cwe476, n_modules, cwe476_model_taxes, real_cwe476_taxes)
 pdf("cwe476.png", height=4, width=7)
 grid.table(df)
 dev.off()
 
 
-df2 = data.frame(kernel_version, n_cwe457, n_modules, cwe457_model_taxes, real_taxes)
+df2 = data.frame(kernel_version, n_cwe457, n_modules, cwe457_model_taxes, real_cwe457_taxes)
 pdf("cwe457.png", height=4, width=7)
-grid.table(df)
+grid.table(df2)
 dev.off()
